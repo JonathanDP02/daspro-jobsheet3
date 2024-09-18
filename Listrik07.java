0a5b3e1cc8df0ec944581e5d88b9050f0c824d0c
JonathanDP02 <jonatanpangestu23@gmail.com>
2024-09-18 22:59:34 +0700

    revisi

--- Listrik07.java
+++ Listrik07.java
@@ -5,20 +5,12 @@ public class Listrik07 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double tarif_per_kwh = 1500.0;
+        int penggunaanListrik, tarif = 1500;
+        boolean batasKwh;
 
-        System.out.print("Masukkan jml penggunaan listruk (kwh): ");
-        int penggunaan = input.nextInt();
+        penggunaanListrik = input.nextInt();
 
-        double tagihan = penggunaan*tarif_per_kwh;
-        System.out.println("Total tagihan anda: Rp " + tagihan);
-
-    if (penggunaan > 500) {
-        System.out.println("Penggunaan anda melebihi 500 kwh");
-    }
-    else {
-        System.out.println("Penggunaan anda tidak melebihi 500 kwh");
-    }
+        System.out.println("Total tagihan = ");
 
     }
     
